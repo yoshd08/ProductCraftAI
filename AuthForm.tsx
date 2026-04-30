6e6bd580148e92a3822277e37260a5e9bdce93c2
yoshd08 <yoshita.dharni@gmail.com>
2026-04-29 20:08:33 -0400

    Auth
    src>components

---
+++ AuthForm.tsx
@@ -0,0 +1,8 @@
+
+'use client';
+
+// This component is no longer used and can be considered removed.
+// Keeping it empty to avoid breaking imports if it were referenced somewhere unexpectedly.
+export default function AuthForm() {
+    return null;
+}
