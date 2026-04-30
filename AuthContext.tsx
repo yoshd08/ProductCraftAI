120cbd445c56445ecbe8a34d9d518b6ad2fa4eb3
yoshd08 <yoshita.dharni@gmail.com>
2026-04-29 20:02:43 -0400

    context
    under src

---
+++ AuthContext.tsx
@@ -0,0 +1,6 @@
+
+'use client';
+
+// This file is effectively removed as it is no longer used by any component.
+// Keeping it empty to avoid breaking imports if it were referenced somewhere unexpectedly.
+export {};
